2b7c177c9c04943f0c4e08356bf20587127c6ed4
David22zambrano <david.zambrano17@outlook.com>
2023-08-29 14:55:07 -0500

    Ajuste clases globales storybook angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,33 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { StandardDirective } from 'src/standard/standard.directive';
 import { MatButtonModule } from '@angular/material/button';
 import { DrawerComponent } from 'src/components/drawer/drawer.component';
-import { SizeDirective } from '@sinco/angular';
-import { RatingComponent } from 'src/components/rating/rating.component';
-import { ChipComponent } from 'src/components/chip/chip.component';
-import { AlertsInfoComponent } from 'src/components/alerts-info/alerts-info.component';
+import { AngularModule } from '@sinco/angular';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
+
+const material= [MatCardModule, MatButtonModule,MatChipsModule]
+
+const componentesPropios = [
+  DrawerComponent
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     StandardDirective,
-    SizeDirective
+    componentesPropios
   ],
   imports: [
-    MatButtonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    DrawerComponent,
-    RatingComponent,
-    ChipComponent,
-    AlertsInfoComponent,
-    SizeDirective
+    NoopAnimationsModule,
+    AngularModule,
+    material
   ],
-  exports: [DrawerComponent],
+  exports: [material,componentesPropios],
   providers: [],
   bootstrap: [AppComponent]
 })
