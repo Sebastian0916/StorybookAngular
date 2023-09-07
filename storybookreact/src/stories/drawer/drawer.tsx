4b2376f5a19aa462f32b89d5f07c4f4de4dcbab4
David22zambrano <david.zambrano17@outlook.com>
2023-09-07 12:41:09 -0500

    Drawer mejora

--- storybookreact/src/stories/drawer/drawer.tsx
+++ storybookreact/src/stories/drawer/drawer.tsx
@@ -5,6 +5,8 @@ import CloseIcon from "@mui/icons-material/Close";
 import Drawer from "@mui/material/Drawer";
 import { DrawerComponentProps } from "./interfaces";
 
+
+
 const stylesDrawerContainer = {
   display: "flex",
   flexDirection: "column",
